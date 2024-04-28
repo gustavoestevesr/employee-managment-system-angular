06e264cce9b78aeaa2bd07d88cd90f610fdc40ff
gustavoestevesr <gustavoesteves@directon.com.br>
2024-04-27 21:30:40 -0300

    refactor: ajuste de URL API e lógica de funções

--- src/app/employees/services/employees.service.ts
+++ src/app/employees/services/employees.service.ts
@@ -1,13 +1,13 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { catchError, delay, first, Observable } from 'rxjs';
+import { catchError, delay, filter, first, Observable } from 'rxjs';
 import { Employee } from './../model/employee';
 
 @Injectable({
   providedIn: 'root',
 })
 export class EmployeesService {
-  private readonly API = '/assets/employees.json';
+  private readonly API = 'http://localhost:3000/employees';
   // private readonly API = '/assets/asdemployees.jpg' // testar tratamento de erro
 
   employees$: Observable<Employee[]> | null = null;
@@ -18,12 +18,12 @@ export class EmployeesService {
     return this.httpClient.get<Employee[]>(this.API).pipe(first(), delay(100));
   }
 
-  loadByCPF(cpf: string) {
-    return this.httpClient.get<Employee>(`${this.API}/${cpf}`).pipe(first());
+  loadByCPF(id: string) {
+    return this.httpClient.get<Employee>(`${this.API}/${id}`).pipe(first());
   }
 
   save(record: Partial<Employee>) {
-    if (record.cpf) {
+    if (record.id) {
       // se o objeto conter um ID vamos atualizar
       return this.update(record);
     } // se não, vamos salvar
@@ -36,11 +36,11 @@ export class EmployeesService {
 
   private update(record: Partial<Employee>) {
     return this.httpClient
-      .put<Employee>(`${this.API}/${record.cpf}`, record)
+      .put<Employee>(`${this.API}/${record.id}`, record)
       .pipe(first());
   }
 
-  remove(cpf: string) {
-    return this.httpClient.delete(`${this.API}/${cpf}`).pipe(first());
+  remove(id: string) {
+    return this.httpClient.delete(`${this.API}/${id}`).pipe(first());
   }
 }
